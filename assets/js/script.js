d8bddfeba68221e454730c72a004f8f29ac59300
ahmell <andrewmell10@gmail.com>
2023-07-23 14:57:54 -0500

    current weather and forecasted weather now display in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 let cityName = "Detroit";
-let weatherCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=26533fca4e43095b1b7c7c281ba63a18`;
+let weatherCurrentURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=26533fca4e43095b1b7c7c281ba63a18`;
+let weatherForecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=26533fca4e43095b1b7c7c281ba63a18`
 let button = document.querySelector('#test');
+let buttonForecast = document.querySelector('#test2');
 
 function getWeather(){
-fetch(weatherCall)
+fetch(weatherCurrentURL)
     .then(function (response) {
         return response.json();
     })
@@ -12,4 +14,15 @@ fetch(weatherCall)
     });
 }
 
+function getForecast(){
+    fetch(weatherForecastURL)
+    .then(function (response){
+        return response.json();
+    })
+    .then (function (data){
+        console.log(data);
+    })
+}
+
 button.addEventListener('click', getWeather());
+buttonForecast.addEventListener('click', getForecast());
